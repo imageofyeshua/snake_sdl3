220d2ddfe63d690be36ca6c2afb667beb8185ca3
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-16 19:43:36 +0900

    Window Init

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_video.h>
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
@@ -9,6 +13,18 @@ static SDL_Renderer *renderer = NULL;
 #define WINDOW_HEIGHT 480
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+  }
+
+  if (!SDL_CreateWindowAndRenderer("Snake", WINDOW_WIDTH, WINDOW_HEIGHT, 0,
+                                   &window, &renderer)) {
+    SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    return SDL_APP_FAILURE;
+  }
+
+  SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+
   return SDL_APP_CONTINUE;
 }
 
