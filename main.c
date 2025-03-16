015a096e15fa667422bd0e3bdfaa9d43ca3d49ea
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-16 19:38:04 +0900

    Window Size Define

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
-#include <SDL3/SDL_init.h>
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
   return SDL_APP_CONTINUE;
